23a09f9573d030afdba0c1ee2bf072e2588ace9f
Irina Bex <92269031+Irchhhh@users.noreply.github.com>
2021-11-24 20:42:52 +0300

    Initial Commit

---
+++ H.W 2.9.Animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  H.W 2.9.Animation
+//
+//  Created by Ирина on 24.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
